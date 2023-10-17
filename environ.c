4cbba174815029f43d6c481e0be12c3da8768995
bonanwaeze@gmail.com <bonanwaeze@gmail.com>
2023-10-17 06:22:11 -0700

    environmental variable

--- environ.c
+++ environ.c
@@ -1,8 +1,13 @@
 #include "main.h"
+/**
+  * env - enviromental variable
+ *
+ */
 void env(void)
 {
 	int i;
-	for (i=0; environ[i]!=NULL;i++)
+
+	for (i = 0; environ[i] != NULL; i++)
 	{
 		write_str(environ[i]);
 		write_str("\n");
