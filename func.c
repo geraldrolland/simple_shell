aa4723877d7def39bc394dc69f5eb1d846818456
bonanwaeze@gmail.com <bonanwaeze@gmail.com>
2023-10-17 17:29:25 -0700

    shell

--- func.c
+++ func.c
@@ -1,67 +1,98 @@
 #include "main.h"
-char *_strdup(char *str) {
+
+/**
+ * _strdup - Duplicates a string.
+ * @str: The string to duplicate.
+ *
+ * Return: Pointer to the duplicated string or NULL if memory allocation fails.
+ */
+char *_strdup(char *str)
+{
 	char *str1;
 	int i, size = 0;
 
-	for (i = 0; str[i] != '\0'; i++) {
+	for (i = 0; str[i] != '\0'; i++)
+	{
 		size++;
 	}
 	size++;
 
 	str1 = malloc(sizeof(char) * size);
 
-	for (i = 0; str[i] != '\0'; i++) {
+	for (i = 0; str[i] != '\0'; i++)
+	{
 		str1[i] = str[i];
 	}
 	str1[i] = '\0';
-	return str1;
+	return (str1);
 }
 
-int _str_len(char *str) {
+/**
+ * _str_len - Calculates the length of a string.
+ * @str: The string to measure.
+ *
+ * Return: The length of the string.
+ */
+int _str_len(char *str)
+{
 	int i, len = 0;
-	for (i = 0; str[i] != '\0'; i++) {
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
 		len++;
 	}
-	return len;
+	return (len);
 }
+
+/**
+ * environ_cmd - Handles environmental commands.
+ * @input: The user input.
+ *
+ * Return: 0 on success, 1 on failure.
+ */
+int environ_cmd(char *input);
 int environ_cmd(char *input)
 {
 	char *token = strtok(input, " \n\r\t");
 	char *argv[100];
-	int i=0;
-	argv[i]=token;
+	int i = 0;
+
+	argv[i] = token;
 	i++;
-	token=strtok(NULL, " \n\r\t");
-	for (; token!=NULL; i++)
+	token = strtok(NULL, " \n\r\t");
+	for (; token != NULL; i++)
 	{
-		argv[i]=token;
-		token=strtok(NULL, " \n\r\t");
+		argv[i] = token;
+		token = strtok(NULL, " \n\r\t");
 	}
-	argv[i]=NULL;
-	if (_strcmp(argv[0], "env")==0 && argv[1]==NULL)
+	argv[i] = NULL;
+	if (_strcmp(argv[0], "env") == 0 && argv[1] == NULL)
 	{
 		env();
 		free(input);
-		return 0;
+		return (0);
 	}
-	if (_strcmp(argv[0], "setenv")==0 && argv[1]!=NULL && argv[2]!=NULL && argv[3]==NULL)
+	if (_strcmp(argv[0], "setenv") == 0 && argv[1] != NULL && argv[2] != NULL
+			&& argv[3] == NULL)
 	{
 		_setenv(argv[1], argv[2]);
 		free(input);
-		return 0;
+		return (0);
 	}
-	if (_strcmp(argv[0], "unsetenv")==0 && argv[1]!=NULL && argv[2]==NULL)
+	if (_strcmp(argv[0], "unsetenv") == 0 && argv[1] != NULL
+			&& argv[2] == NULL)
 	{
 		_unsetenv(argv[1]);
 		free(input);
-		return 0;
+		return (0);
 	}
-	if (_strcmp(argv[0], "cd") == 0 && (argv[1]==NULL || argv[1]!=NULL) && argv[2]==NULL)
+	if (_strcmp(argv[0], "cd") == 0 && (argv[1] == NULL || argv[1] != NULL)
+			&& argv[2] == NULL)
 	{
 		call_cd(argv[1]);
 		free(input);
-		return 0;
+		return (0);
 	}
 	free(input);
-	return 1;
+	return (1);
 }
