73f2f9a5dc581be02d674ce3a55ecf2729807faa
bonanwaeze@gmail.com <bonanwaeze@gmail.com>
2023-10-17 17:30:12 -0700

    shell

--- shell.c
+++ shell.c
@@ -1,12 +1,19 @@
 #include "main.h"
 int no_input(char *input);
-int handle_cmd (char *input_copy3, char *input_copy);
+int handle_cmd(char *input_copy3, char *input_copy);
+
+/**
+ * main - The main function.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char *input=NULL;
+	char *input = NULL;
 	size_t size = 0;
-	int isterminal=0;
-	char *input_copy2, *input_copy3,*tok, *input_copy;
+	int isterminal = 0;
+	char *input_copy2, *input_copy3, *tok, *input_copy;
+
 	while (1)
 	{
 		if (isatty(isterminal) != 0)
@@ -26,77 +33,116 @@ int main(void)
 			free(input_copy2);
 			break;
 		}
-		if (no_input(input_copy2)==0)
+		if (no_input(input_copy2) == 0)
 		{
 			free(input_copy3);
 			continue;
 		}
 		input_copy = _strdup(input);
-		if (handle_cmd(input_copy3, input_copy)==0)
+		if (handle_cmd(input_copy3, input_copy) == 0)
 			continue;
 		if (isterminal != 0)
 			break;
 		continue;
 	}
-	return 0;
+	return (0);
 }
-int handle_cmd (char *input_copy3, char *input_copy)
+
+/**
+ * handle_cmd - Handles the command input.
+ * @input_copy3: Copy of the input.
+ * @input_copy: Copy of the input.
+ *
+ * Return: 0 on success, 1 on failure.
+ */
+int handle_cmd(char *input_copy3, char *input_copy)
 {
 	struct stat buffer;
 	char *userinput = _strdup(input_copy);
-	if (environ_cmd(userinput)==0)
+
+	if (environ_cmd(userinput) == 0)
 	{
 		free(input_copy3);
 		free(input_copy);
-		return 0;
+		return (0);
 	}
 	if (compare_path(input_copy3) == 0)
 	{
 		free(input_copy);
-		return 0;
+		return (0);
 	}
-	if (stat(input_copy, &buffer)==-1)
+	if (stat(input_copy, &buffer) == -1)
 	{
 		free(input_copy);
 		perror("./hsh");
 	}
-	return 1;
+	return (1);
 }
+
+/**
+ * _strcmp - Compares two strings.
+ * @str1: The first string.
+ * @str2: The second string.
+ *
+ * Return: 0 if equal, 1 otherwise.
+ */
 unsigned int _strcmp(char *str1, char *str2)
 {
 	while (*str1 != '\0' && *str2 != '\0')
 	{
 		if (*str1 != *str2)
 		{
-			return 1;
+			return (1);
 		}
 		str1++;
 		str2++;
 	}
-	return 0;
+	return (0);
 }
 
+/**
+ * _strlen - Calculates the length of a string.
+ * @str: The input string.
+ *
+ * Return: The length of the string.
+ */
 unsigned int _strlen(const char *str)
 {
 	unsigned int count = 0;
+
 	while (*str != '\0')
 	{
 		count++;
 		str++;
 	}
-	return count;
+	return (count);
 }
 
+
+/**
+ * write_str - Writes a string to standard output.
+ * @str: The string to write.
+ *
+ * Return: Always 0.
+ */
 int write_str(char *str)
 {
 	write(STDOUT_FILENO, str, _strlen(str));
-	return 0;
+	return (0);
 }
+
+/**
+ * no_input - Checks if there is any input.
+ * @input: The input string.
+ *
+ * Return: 0 if no input, 1 otherwise.
+ */
 int no_input(char *input)
 {
-	int count=0;
+	int count = 0;
 	char *token = strtok(input, " \n\r\t");
-	for (; token!=NULL;)
+
+	for (; token != NULL;)
 	{
 		count++;
 		token = strtok(NULL, " \n\r\t");
@@ -105,8 +151,8 @@ int no_input(char *input)
 	free(input);
 	if (count == 1)
 	{
-		return 0;
+		return (0);
 	}
-	return 1;
-	
+	return (1);
+
 }
